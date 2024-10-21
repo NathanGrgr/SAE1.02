3d98d4363134762f1358e85c64d357bc5e78d008
NathanGrgr <126341708+NathanGrgr@users.noreply.github.com>
2024-10-21 12:06:02 +0200

    Add files via upload

--- Système_Votes/Vote_classement/classement.py
+++ Système_Votes/Vote_classement/classement.py
@@ -4,38 +4,53 @@ from random import *
 #Chaque votant fait un classement de tous les candidats
 
 
-scrutin=[]
+
 votant=1000
+candidat=5
 
+def scrutin(candidat):
+    scrutin=[]
+    for i in range(votant):
+        scrutin.append(sample(range(1,candidat+1),candidat))
+    return scrutin
 
-def trouver_nombre_frequent(liste):
-    max_count = 0
-    nombre_frequent = liste[0]
-
-    # Parcourir chaque élément unique de la liste
-    for nombre in liste:
-        count = 0
-        # Compter combien de fois cet élément apparaît dans la liste
-        for i in liste:
-            if i == nombre:
-                count += 1
-        # Mettre à jour si un élément plus fréquent est trouvé
-        if count > max_count:
-            max_count = count
-            nombre_frequent = nombre
-
-    # Retourner le nombre le plus fréquent
-    return nombre_frequent
+def election(scrutin):
+    elec=[0]*candidat
+    for i in range(len(scrutin)):
+        nbr=scrutin[i][0]
+        elec[nbr-1]+=1
+    return(elec)
 
 
 
-for i in range(votant):
+def test(liste):
+    new_liste=[]
+    for i in range(len(liste)):
+        if liste[i]!=0:
+            new_liste.append(liste[i])
+    return new_liste
+
+def majorite(elec,candidat):
+    for i in range(len(elec)):
+        if elec[i] >= votant/2 or len(elec)==2:
+            print(elec)
+            print("Candidat",elec.index(max(elec))+1,"Gagnant de l'élection")
+            return
+
+    temp_elec=test(elec)
+    der=elec.index(min(temp_elec))+1
+    candidat-=1
+    print("Elimination du",der,"e candidat")
+    print(elec[der-1],elec,candidat)
     
+    scru=scrutin(candidat)
+    elec=election(scru)
+    majorite(elec,candidat)
 
-    for j in range(5):
-        choix_votant=[1,2,3,4,5]
-        shuffle(choix_votant)
-        #Faire équivalent de shuffle en algo et en C++
-    scrutin.append(choix_votant)
 
-print(scrutin)
+
+
+if __name__=="__main__":
+    scru=scrutin(candidat)
+    elec=election(scru)
+    majorite(elec,candidat)
