bad82245bbea98571abec561a4a54d1d0771ac82
NathanGrgr <126341708+NathanGrgr@users.noreply.github.com>
2024-11-10 23:07:23 +0100

    Add files via upload

--- Système_Votes/Vote_Majorite/main.cpp
+++ Système_Votes/Vote_Majorite/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Vote à majorité simple à 2 tours
+ * Le candidat ayant le plus de voies gagne au 2nd tour
+ * Candidat peut gagner au 1er tour si majorité
+ */
+
+
 #include <iostream>
 #include <vector>
 
@@ -12,56 +19,20 @@ string litUneString (){
     return uneChaine;
 }
 
-// vector<int> convertstrintovector(const string& chaine){
-//     string newchaine = chaine.substr(1, chaine.size()-2);
-//     vector<int> vector;
-//     string element;
-//     for (char ch : newchaine){
-//         if (ch == ','){
-//             vector.push_back(stoi(element));
-//             element = "";
-//         }
-//         else {
-//             element += ch;
-//         }
-//     }
-//     vector.push_back(stoi(element));
-//     return vector;
-// }
-
-vector<int> convertstrintovector(const string& chaine) {
-    if (chaine.size() < 2 || chaine.front() != '[' || chaine.back() != ']') {
-        throw invalid_argument("Invalid format: Expected format [1,2,3]");
-    }
-
-    string newchaine = chaine.substr(1, chaine.size() - 2);
+vector<int> convertstrintovector(const string& chaine){
+    string newchaine = chaine.substr(1, chaine.size()-2);
     vector<int> vector;
     string element;
-
-    for (char ch : newchaine) {
-        if (ch == ',') {
-            if (!element.empty()) {
-                try {
-                    vector.push_back(stoi(element));
-                } catch (const invalid_argument& e) {
-                    cerr << "Invalid integer: " << element << endl;
-                }
-                element.clear();
-            }
-        } else {
+    for (char ch : newchaine){
+        if (ch == ','){
+            vector.push_back(stoi(element));
+            element = "";
+        }
+        else {
             element += ch;
         }
     }
-
-    // Push the last element after the loop
-    if (!element.empty()) {
-        try {
-            vector.push_back(stoi(element));
-        } catch (const invalid_argument& e) {
-            cerr << "Invalid integer: " << element << endl;
-        }
-    }
-
+    vector.push_back(stoi(element));
     return vector;
 }
 
@@ -93,7 +64,7 @@ void affichVectInt (const vector<int> & v){
 
 
 
-vector<int> election(const vector<vector<int>>& classement){
+vector<int> election(const vector<vector<int>>& classement,vector<string> vCandidat){
     size_t votants = classement.size();
     int seuil = votants/2;
     vector<int> VoteDecompte(4,0);
@@ -116,6 +87,10 @@ vector<int> election(const vector<vector<int>>& classement){
             return VoteDecompte;
         }
     }
+    cout << "Début du 1er tour : " << endl;
+    for (size_t i(0); i < VoteDecompte.size(); ++i){
+        cout << "Le candidat " << vCandidat[i] << " a obtenu " << VoteDecompte[i] << " votes" << endl;
+    }
     VoteDecompte.clear();
     return VoteDecompte;
 }
@@ -129,18 +104,16 @@ vector<int> election2etour(const vector<vector<int>>& classement){
         if (classement[i][1]==1){
             Vote2eDecompte[0]++;
         }
-        else if (classement[i][2]==2){
+        else if (classement[i][1]==2){
             Vote2eDecompte[1]++;
         }
-        else if (classement[i][0]==3){
+        else if (classement[i][1]==3){
             Vote2eDecompte[2]++;
         }
-        else if (classement[i][0]==4){
+        else if (classement[i][1]==4){
             Vote2eDecompte[3]++;
         }
     }
-    affichVectInt(Vote2eDecompte);
-
 
     return Vote2eDecompte;
 }
@@ -155,15 +128,16 @@ void affichVectParticipants (const vector<participant> & vPart){
     for (const participant & part : vPart){
         cout << part.nom << endl;
         cout << part.prenom << endl;
+        affichVectInt(part.classement);
     }
 }
 
 int main()
 {
     vector <string> vCandidat;
-    for (unsigned i (0); i < 3; ++i)
+    for (unsigned i (0); i < 4; ++i)
         vCandidat.push_back(litUneString());
-    affichVectString (vCandidat);
+    //affichVectString (vCandidat);
 
     vector<participant> vParticipant;
 
@@ -176,14 +150,15 @@ int main()
     }
 
     vector<vector<int>> classementParticipants;
+
     for (const auto& part : vParticipant)
         classementParticipants.push_back(part.classement);
 
-
+    //[[1,2][..]..]
     vector<int> listeelection;
-    listeelection= election(classementParticipants);
+    listeelection= election(classementParticipants,vCandidat);
     int max_indice = 0;
-    int max = listeelection[0];
+
     if (listeelection.size()!=0){
         int max = listeelection[0];
 
@@ -193,38 +168,34 @@ int main()
                 max = listeelection[i];
                 max_indice = i;
             }
-        cout << "Le candidat " << max_indice +1 << " est élu avec " << max << " des voies au 1er tour sur un total de " << votants << " voies !" << endl;
+        cout << "Le candidat " << vCandidat[max_indice] << " est élu avec " << max << " des voies au 1er tour sur un total de " << votants << " voies !" << endl;
+            return 0;
         }
     }
     else{
+        cout << "Aucun candidat n'a gagné à la majorité" << endl;
+        cout << "Debut du 2e tour : " << endl;
         listeelection=election2etour(classementParticipants);
+        //affichVectInt(listeelection);
         int max = listeelection[0];
 
 
+        for (size_t i = 0; i < listeelection.size(); ++i){
+            cout << "Le candidat " << vCandidat[i] << " a obtenu " << listeelection[i] << " votes" << endl;
+        }
+
+
         for (size_t i = 1; i < listeelection.size(); ++i) {
+
+
             if (listeelection[i] > max) {
+
                 max = listeelection[i];
                 max_indice = i;
             }
-        cout << "Le candidat " << max_indice +1 << " est élu avec " << max << " des voies au er tour sur un total de " << votants << " voies !" << endl;
+        cout << "Le candidat " << vCandidat[max_indice] << " est élu avec " << max << " voies au 2er tour sur un total de " << votants << " voies !" << endl;
+        return 0;
         }
+
     }
-
-
-
-    //     affichVectParticipants(vParticipant);
-    //     cout << string (15, '-') << endl;
-    //     sort (vParticipant.begin(), vParticipant.end(), compare2part);
-    //     affichVectParticipants(vParticipant);
-    // // */
-
-    //     cout << "c'est la glace " << vGlacePref[0] << " qui a gagne" << endl;
-    return 0;
 }
-
-/*
-Nom
-Prénon
-Choix1
-Choix2
-*/
