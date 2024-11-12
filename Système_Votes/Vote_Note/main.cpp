d49a39cfcd4a2b15d3d4ba5558cf6891dad5e62c
NathanGrgr <126341708+NathanGrgr@users.noreply.github.com>
2024-11-12 22:23:07 +0100

    Create main.cpp

---
+++ Système_Votes/Vote_Note/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string litUneString (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return uneChaine;
+}
+
+
+int litUnEntier (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return stoi(uneChaine);
+}
+
+void affichVectInt (const vector<int> & v){
+    for (const int & val : v)
+        cout << val << ' ';
+    cout << endl;
+}
+
+void affichVectFloat(const vector<float>& vec) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << vec[i] << " ";  // Affiche chaque élément suivi d'un espace
+    }
+    cout << endl;  // Saute une ligne à la fin
+}
+vector<float> election (const vector<int> & NoteParticipant, const vector<int> & CandidatParticipant){
+    vector<float> CompteurNote(4, 0.0f);
+    int val = CandidatParticipant[0];
+    for (size_t i(0); i < NoteParticipant.size(); ++i){
+        val = CandidatParticipant[i]-1;
+        //cout << NoteParticipant[i] << endl;
+        CompteurNote[val]+=static_cast<float>(NoteParticipant[i]);
+    }
+    //affichVectInt(CompteurNote);
+    for (size_t i(0); i < CompteurNote.size(); ++i){
+        CompteurNote[i]=CompteurNote[i]/29;
+    }
+    return CompteurNote;
+}
+struct participant {
+    string nom;
+    string prenom;
+    int candidat;
+    int note;
+};
+
+
+
+
+
+void affichVectString (const vector<string> & v){
+    for (const string & val : v)
+        cout << val << '\t';
+    cout << endl;
+}
+
+void affichVectParticipants (const vector<participant> & vPart){
+    for (const participant & part : vPart){
+        cout << part.nom << endl;
+        cout << part.prenom << endl;
+        cout << part.candidat << endl;
+        cout << part.note << endl;
+    }
+}
+
+
+int main()
+{
+    vector <string> vCandidat;
+    for (unsigned i (0); i < 4; ++i)
+        vCandidat.push_back(litUneString());
+    //affichVectString (vCandidat);
+
+    vector<participant> vParticipant;
+
+    unsigned votants = 116;
+    for (unsigned i (0); i < votants; ++i){
+        string nom (litUneString());
+        string prenom  (litUneString());
+        int candidat (litUnEntier());
+        int note(litUnEntier());
+        vParticipant.push_back(participant{nom, prenom, candidat,note});
+    }
+
+    vector<int> NoteParticipant;
+    for (const auto& part : vParticipant)
+        NoteParticipant.push_back(part.note);
+
+    vector<int> CandidatParticipant;
+    for (const auto& part : vParticipant)
+        CandidatParticipant.push_back(part.candidat);
+
+    //affichVectInt(NoteParticipant);
+    //affichVectInt(CandidatParticipant);
+    //[4,5,4,1,10,2]
+    vector<float> listeelection;
+    listeelection=election(NoteParticipant,CandidatParticipant);
+    //affichVectFloat(listeelection);
+
+    float max = listeelection[0];
+    int max_indice = 0;
+
+    for (size_t i = 1; i < listeelection.size(); ++i) {
+        if (listeelection[i] > max) {
+            max = listeelection[i];
+            max_indice = i;
+        }
+    }
+
+    cout << "Le candidat " << vCandidat[max_indice] << " est élu avec un score de " << max << " sur 10 avec un effectif de " << votants << " voies !" << endl;
+    return 0;
+}
+
